77cdd2ae47e819d7cef02a372162e5f4aa51a202
rutujapatil63 <86056830+rutujapatil63@users.noreply.github.com>
2021-06-24 20:21:44 +0530

    Update workbookUiComponent.js

--- force-app/main/default/lwc/workbookUiComponent/workbookUiComponent.js
+++ force-app/main/default/lwc/workbookUiComponent/workbookUiComponent.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track,wire} from 'lwc';
+import getprofiles from '@salesforce/apex/getinfo.getprofiles';
 
-export default class WorkbookUiComponent extends LightningElement {}
+export default class WorkbookUiComponent extends LightningElement {
+   
+    @track openModal = false;
+    showModal() {
+        this.openModal = true;
+    }
+    closeModal() {
+        this.openModal = false;
+    }
+    
+}
